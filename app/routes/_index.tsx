e79759d59ac9bf76acbf0491e2ec39a78ad48b9f
schmeekygeek <ss6087982@gmail.com>
2024-12-31 20:32:44 +0530

    scale on hover and upload component

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,22 @@
-import { Button } from "~/components/ui/button";
 import { Link } from "@remix-run/react";
 import { ThemeToggle } from "./resources.theme-toggle";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/components/ui/card";
-import { ArrowRight } from "lucide-react";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import UploadContent, { FileType } from "~/components/upload-file";
+import { useState } from "react";
+import { Button } from "~/components/ui/button";
+
 
 export default function Index() {
+
+  const [fileType, setFileType] = useState<FileType>("");
+
   return (
     <section className="w-full min-h-screen flex flex-col">
       <nav className="flex items-center justify-between p-4 w-full">
@@ -21,48 +33,69 @@ export default function Index() {
         <div className="flex-col justify-end items-center space-y-4 p-4">
           <h1 className="text-balance text-4xl md:text-6xl font-bold tracking-tighter">
             An open-source tool to compress an{" "}
-            <span className="font-extrabold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">
+            <span
+              className="font-extrabold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient"
+            >
               image
             </span>{" "}
             or{" "}
-            <span className="font-extrabold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">
+            <span
+              className="font-extrabold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient"
+            >
               PDF
             </span>{" "}
-            <Link
-              to="https://ui.aceternity.com"
-              className="hover:text-blue-500"
-            >
-              with ease.
-            </Link>
+            with ease.
           </h1>
           <p className="text-slate-500 dark:text-slate-200 text-lg">
             Make files portable and save bandwidth when sharing files online.
           </p>
           <div className="h-5"/>
+          {fileType === "" ? 
           <div className="flex-1 flex-col justify-center items-start">
-            <Card className="flex justify-between items-end my-3 md:w-96">
-              <CardHeader>
-                <CardTitle
-                  className="text-2xl"
-                >Pick an image</CardTitle>
-                <CardDescription className="text-md">Pick any jpeg or png image</CardDescription>
-              </CardHeader>
-              <CardFooter>
-                <ArrowRight />
-              </CardFooter>
-            </Card>
-            <Card className="flex justify-between items-end my-3 md:w-96">
-              <CardHeader>
-                <CardTitle
-                  className="text-2xl"
-                >Pick a PDF</CardTitle>
-                <CardDescription className="text-md">Pick a PDF format file</CardDescription>
-              </CardHeader>
-              <CardFooter>
-                <ArrowRight />
-              </CardFooter>
-            </Card>
+            <div onClick={() => setFileType("image")}>
+              <Card className="flex justify-between items-end my-3 md:w-96 transition transform hover:scale-105 hover:cursor-pointer">
+                <CardHeader>
+                  <CardTitle
+                    className="text-2xl"
+                  >Pick an image</CardTitle>
+                  <CardDescription className="text-md">Pick any jpeg or png image</CardDescription>
+                </CardHeader>
+                <CardFooter>
+                  <ArrowRight />
+                </CardFooter>
+              </Card>
+            </div>
+            <div
+              onClick={() => setFileType("pdf")}
+            >
+              <Card
+                className="flex justify-between items-end my-3 md:w-96 transition transform hover:scale-105 hover:cursor-pointer"
+              >
+                <CardHeader>
+                  <CardTitle
+                    className="text-2xl"
+                  >Pick a PDF</CardTitle>
+                  <CardDescription className="text-md">Pick a PDF format file</CardDescription>
+                </CardHeader>
+                <CardFooter>
+                  <ArrowRight />
+                </CardFooter>
+              </Card>
+            </div>
           </div>
+          : 
+            <div>
+              <Button
+                onClick={() => setFileType("")}
+                className="my-4"
+                variant="ghost"
+              >
+                <ArrowLeft className="pe-2 h-4"/>
+                Go Back
+              </Button>
+              <UploadContent fileType={fileType}/>
+            </div>
+          }
         </div>
       </div>
     </section>
