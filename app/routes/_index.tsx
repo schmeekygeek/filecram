c3a049b8a8bf6967dc1714b948b213c10017e048
schmeekygeek <ss6087982@gmail.com>
2024-12-31 12:20:01 +0530

    update home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,61 +1,67 @@
 import { Button } from "~/components/ui/button";
 import { Link } from "@remix-run/react";
-import { Command } from "lucide-react";
-import { Card, CardContent } from "~/components/ui/card";
 import { ThemeToggle } from "./resources.theme-toggle";
-import { Hero3DCard } from "~/components/hero-3d-card";
-import { TextRevealCardPreview } from "~/components/text-reveal-preview";
-import { AnimatedTooltipPreview } from "~/components/animated-preview";
-import { HeroParallaxDemo } from "~/components/parallax-preview";
-import { AnimatedPinDemo } from "~/components/animated-3d-pin";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/components/ui/card";
+import { ArrowRight } from "lucide-react";
 
 export default function Index() {
   return (
     <section className="w-full min-h-screen flex flex-col">
       <nav className="flex items-center justify-between p-4 w-full">
         <Link to="/" className="flex items-center space-x-2">
-          <Command className="h-8 w-8" />
-          <h1 className="text-xl font-semibold">Remix + Shadcn</h1>
+          <span className="font-extrabold text-2xl bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">
+            <h1>
+              FileCram
+            </h1>
+          </span>{" "}
         </Link>
         <ThemeToggle />
       </nav>
-      <div className="container flex justify-center px-4 md:px-6 flex-1 py-8 overflow-x-hidden">
-        <div className="flex flex-col items-center space-y-4 text-center p-4 md:w-1/2">
-          <h1 className="text-3xl md:text-4xl font-bold tracking-tighter">
-            A{" "}
+      <div className="flex-col justify-center">
+        <div className="flex-col justify-end items-center space-y-4 p-4">
+          <h1 className="text-balance text-4xl md:text-6xl font-bold tracking-tighter">
+            An open-source tool to compress an{" "}
             <span className="font-extrabold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">
-              Simple Starter
+              image
             </span>{" "}
-            For Remix, Shadcn-ui and{" "}
+            or{" "}
+            <span className="font-extrabold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">
+              PDF
+            </span>{" "}
             <Link
               to="https://ui.aceternity.com"
               className="hover:text-blue-500"
             >
-              Aceternity-ui
+              with ease.
             </Link>
           </h1>
-
-          <p className="text-muted-foreground font-bold mt-2">
-            With optimistic dark-mode.
+          <p className="text-slate-500 dark:text-slate-200 text-lg">
+            Make files portable and save bandwidth when sharing files online
           </p>
-          <p className="text-muted-foreground mt-2">
-            (On mobile, tap for animations.)
-          </p>
-
-          <Card className="relative group overflow-hidden rounded-lg">
-            <CardContent className="p-1 bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 bg-300% animate-gradient">
-              <Button asChild>
-                <Link to="https://github.com/rajeshdavidbabu/remix-shadcn-starter">
-                  Star on Github
-                </Link>
-              </Button>
-            </CardContent>
-          </Card>
-
-          <div className="px-4">
-            <AnimatedTooltipPreview />
-            <AnimatedPinDemo />
-            <Hero3DCard />
+          <div className="h-5"/>
+          <div className="flex-1 flex-col justify-center items-start">
+            <Card className="flex justify-between items-end my-3 md:w-96">
+              <CardHeader>
+                <CardTitle
+                  className="text-2xl"
+                >Pick an image</CardTitle>
+                <CardDescription className="text-md">Pick any jpeg or png image</CardDescription>
+              </CardHeader>
+              <CardFooter>
+                <ArrowRight />
+              </CardFooter>
+            </Card>
+            <Card className="flex justify-between items-end my-3 md:w-96">
+              <CardHeader>
+                <CardTitle
+                  className="text-2xl"
+                >Pick a PDF</CardTitle>
+                <CardDescription className="text-md">Pick a PDF format file</CardDescription>
+              </CardHeader>
+              <CardFooter>
+                <ArrowRight />
+              </CardFooter>
+            </Card>
           </div>
         </div>
       </div>
