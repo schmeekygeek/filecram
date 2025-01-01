2d56cf74bcf03060817d8dde7f3e7c51ccb10150
LeonMendonca <leonmendonca619@gmail.com>
2025-01-01 14:15:27 +0530

    reuse components

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,17 @@
-import { Button } from "~/components/ui/button";
 import { Link } from "@remix-run/react";
 import { ThemeToggle } from "./resources.theme-toggle";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/components/ui/card";
-import { ArrowRight } from "lucide-react";
+import GrandientText from "~/components/reuse-components/grandient-text";
+import CardReuseComp from '~/components/reuse-components/card'
 
 export default function Index() {
   return (
     <section className="w-full min-h-screen flex flex-col">
       <nav className="flex items-center justify-between p-4 w-full">
         <Link to="/" className="flex items-center space-x-2">
-          <span className="font-extrabold text-2xl bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">
+          <span 
+            className="font-extrabold text-3xl bg-gradient-to-r from-orange-700 via-blue-500 
+            to-green-400 text-transparent bg-clip-text bg-300% animate-gradient"
+          >
             <h1>
               FileCram
             </h1>
@@ -21,13 +23,9 @@ export default function Index() {
         <div className="flex-col justify-end items-center space-y-4 p-4">
           <h1 className="text-balance text-4xl md:text-6xl font-bold tracking-tighter">
             An open-source tool to compress an{" "}
-            <span className="font-extrabold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">
-              image
-            </span>{" "}
+            <GrandientText text="image" tag="span"/>
             or{" "}
-            <span className="font-extrabold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">
-              PDF
-            </span>{" "}
+            <GrandientText text="PDF" tag="span"/>
             <Link
               to="https://ui.aceternity.com"
               className="hover:text-blue-500"
@@ -39,29 +37,19 @@ export default function Index() {
             Make files portable and save bandwidth when sharing files online.
           </p>
           <div className="h-5"/>
-          <div className="flex-1 flex-col justify-center items-start">
-            <Card className="flex justify-between items-end my-3 md:w-96">
-              <CardHeader>
-                <CardTitle
-                  className="text-2xl"
-                >Pick an image</CardTitle>
-                <CardDescription className="text-md">Pick any jpeg or png image</CardDescription>
-              </CardHeader>
-              <CardFooter>
-                <ArrowRight />
-              </CardFooter>
-            </Card>
-            <Card className="flex justify-between items-end my-3 md:w-96">
-              <CardHeader>
-                <CardTitle
-                  className="text-2xl"
-                >Pick a PDF</CardTitle>
-                <CardDescription className="text-md">Pick a PDF format file</CardDescription>
-              </CardHeader>
-              <CardFooter>
-                <ArrowRight />
-              </CardFooter>
-            </Card>
+          <div className="flex flex-wrap py-4 gap-x-4 justify-center items-start">
+            <CardReuseComp cardTitle = "Pick an Image" cardDesc = "Pick any jpeg or png image"/>
+            <CardReuseComp cardTitle = "Pick a PDF" cardDesc = "Pick a PDF format file"/>
+            <CardReuseComp cardTitle = "Pick a PDF" cardDesc = "Pick a PDF format file"/>
+            <CardReuseComp cardTitle = "Pick a PDF" cardDesc = "Pick a PDF format file"/>
+            <CardReuseComp cardTitle = "Pick a PDF" cardDesc = "Pick a PDF format file"/>
+            <CardReuseComp cardTitle = "Pick a PDF" cardDesc = "Pick a PDF format file"/>
+            <CardReuseComp cardTitle = "Pick a PDF" cardDesc = "Pick a PDF format file"/>
+            <CardReuseComp cardTitle = "Pick a PDF" cardDesc = "Pick a PDF format file"/>
+            <CardReuseComp cardTitle = "Pick a PDF" cardDesc = "Pick a PDF format file"/>
+            <CardReuseComp cardTitle = "Pick a PDF" cardDesc = "Pick a PDF format file"/>
+            <CardReuseComp cardTitle = "Pick a PDF" cardDesc = "Pick a PDF format file"/>
+            <CardReuseComp cardTitle = "Pick a PDF" cardDesc = "Pick a PDF format file"/>
           </div>
         </div>
       </div>
