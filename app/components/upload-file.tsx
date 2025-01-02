a44c278d890d73006b0f86bdf85716c049eecffb
Leon Mendonca <96129122+LeonMendonca@users.noreply.github.com>
2025-01-02 12:28:47 +0530

    Revert "file picker can now pick a file with specific format"

--- app/components/upload-file.tsx
+++ app/components/upload-file.tsx
@@ -7,20 +7,9 @@ interface UploadContentProps {
 export type FileType = "image" | "pdf" | "";
 
 export default function UploadContent(props: UploadContentProps) {
-  let type = ""
-  switch(props.fileType) {
-    case 'image':
-      type="image/png, image/jpeg"
-    break;
-    case 'pdf':
-      type=".pdf"
-    break;
-    default:
-      type=""
-  }
   return (
     <div className="w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-white dark:bg-black border-neutral-200 dark:border-neutral-800 rounded-lg">
-    <FileUpload filetype={type}/>
+      <FileUpload />
     </div>
 
   )
